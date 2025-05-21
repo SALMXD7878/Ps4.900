975167fb5b00acfd1f3e6e1665ee30a847fda3cd
SALMXD7878 <smm2856111@gmail.com>
2025-05-21 14:44:31 +0300

    Update psfree.mjs

--- psfree.mjs
+++ psfree.mjs
@@ -33,10 +33,10 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.  */
 //   * Helped in figuring out the size of JSC::ArrayBufferContents and its
 //     needed offsets on different firmwares (PS5).
 
-import { Int } from './module/int64.mjs';
-import { Memory } from './module/mem.mjs';
-import { KB, MB } from './module/offset.mjs';
-import { BufferView } from './module/rw.mjs';
+import { Int } from './module/int64.js';
+import { Memory } from './module/mem.js';
+import { KB, MB } from './module/offset.js';
+import { BufferView } from './module/rw.js';
 
 import {
     die,
@@ -46,13 +46,34 @@ import {
     sleep,
     hex,
     align,
-} from './module/utils.mjs';
+} from './module/utils.js';
 
-import * as config from './config.mjs';
-import * as off from './module/offset.mjs';
+import * as config from './config.js';
+import * as off from './module/offset.js';
+
+addEventListener('unhandledrejection', event => {
+    const reason = event.reason;
+    alert(
+        'Unhandled rejection\n'
+        + `${reason}\n`
+        + `${reason.sourceURL}:${reason.line}:${reason.column}\n`
+        + `${reason.stack}`
+    );
+});
+
+addEventListener('error', event => {
+    const reason = event.error;
+    alert(
+        'Unhandled error\n'
+        + `${reason}\n`
+        + `${reason.sourceURL}:${reason.line}:${reason.column}\n`
+        + `${reason.stack}`
+    );
+    return true;
+});
 
 // check if we are running on a supported firmware version
-const [is_ps4, version] = (() => {
+/*const [is_ps4, version] = (() => {
     const value = config.target;
     const is_ps4 = (value & 0x10000) === 0;
     const version = value & 0xffff;
@@ -69,9 +90,9 @@ const [is_ps4, version] = (() => {
     }
 
     return [is_ps4, version];
-})();
+})();*/
 
-const ssv_len = (() => {
+/*const ssv_len = (() => {
     if (0x600 <= config.target && config.target < 0x650) {
         return 0x58;
     }
@@ -84,9 +105,10 @@ const ssv_len = (() => {
     if (0x650 <= config.target && config.target < 0x900) {
         return 0x48;
     }
-})();
+})();*/
 
 // these constants are expected to be divisible by 2
+const ssv_len = 0x50;
 const num_fsets = 0x180;
 const num_spaces = 0x40;
 const num_adjs = 8;
@@ -524,7 +546,8 @@ async function leak_code_block(reader, bt_size) {
         cache.push(part + `var idx = ${i};\nidx\`foo\`;`);
     }
 
-    const chunkSize = (is_ps4 && version < 0x900) ? 128 * KB : 1 * MB;
+    //const chunkSize = (is_ps4 && version < 0x900) ? 128 * KB : 1 * MB;
+    const chunkSize = 128 * KB;
     const smallPageSize = 4 * KB;
     const search_addr = align(rdr.m_data, chunkSize);
     log(`search addr: ${search_addr}`);
@@ -609,14 +632,14 @@ async function leak_code_block(reader, bt_size) {
 // address
 function make_ssv_data(ssv_buf, view, view_p, addr, size) {
     // sizeof JSC::ArrayBufferContents
-    const size_abc = (() => {
+    /*const size_abc = (() => {
         if (is_ps4) {
             return version >= 0x900 ? 0x18 : 0x20;
         } else {
             return version >= 0x300 ? 0x18 : 0x20;
         }
-    })();
-
+    })();*/
+    const size_abc = 0x900 ? 0x18 : 0x20;
     const data_len = 9;
     // sizeof WTF::Vector<T>
     const size_vector = 0x10;
@@ -857,6 +880,8 @@ async function main() {
 
     clear_log();
     // path to your script that will use the exploit
-    import('./lapse.mjs');
+    import('./lapse.js');
 }
-main();
+//main();
+setTimeout(main, 1500);
+setTimeout(StartTimer, 1500);
