0620b8fc167ec1dbc29bc6f3958eb316d6444c92
SALMXD7878 <smm2856111@gmail.com>
2025-05-21 14:39:40 +0300

    Update MakeCache.bat

--- MakeCache.bat
+++ MakeCache.bat
@@ -1,40 +1,41 @@
 @echo off
 
-echo CACHE MANIFEST > temp_manifest.txt
-echo. >> temp_manifest.txt
-echo CACHE: >> temp_manifest.txt
+echo CACHE MANIFEST > test0.txt
+echo #VERSION 1.1 >> test0.txt
+echo. >> test0.txt
+echo CACHE: >> test0.txt
 
 set LOC=%~dp0
 
-dir /B /S /A:-D >> temp_manifest.txt
+dir /B /S /A:-D >> test0.txt
 
-echo. >> temp_manifest.txt
-echo NETWORK: >> temp_manifest.txt
-echo * >> temp_manifest.txt
+echo. >> test0.txt
+echo NETWORK: >> test0.txt
+echo * >> test0.txt
 
-findstr /v "media .bat .exe .mp4 .git .py temp_manifest.txt" temp_manifest.txt > clean_files.txt
-del temp_manifest.txt
+findstr /v "media .bat .exe .mp4 .git .py test0.txt" test0.txt > test.txt
+del test0.txt
 
 @echo off
 setlocal enableextensions disabledelayedexpansion
 set "search=%LOC%"
 set "replace="
-set "textFile=clean_files.txt"
+set "textFile=test.txt"
 for /f "delims=" %%i in ('type "%textFile%" ^& break ^> "%textFile%" ') do (
-    set "line=%%i"
-    setlocal enabledelayedexpansion
-    >>"%textFile%" echo(!line:%search%=%replace%!
-    endlocal
-)
+set "line=%%i"
+setlocal enabledelayedexpansion
+>>"%textFile%" echo(!line:%search%=%replace%!
+endlocal)
 
 setlocal DisableDelayedExpansion
 set "firstLineReady="
-(for /F "eol=$ delims=" %%a in (clean_files.txt) DO (
-    if defined firstLineReady (echo()
-    set "firstLineReady=1"
-    <nul set /p "=%%a"
-)) > psfreelapse.manifest
-del clean_files.txt
+(for /F "eol=$ delims=" %%a in (test.txt) DO (
+if defined firstLineReady (echo()
+set "firstLineReady=1"
+<nul set /p "=%%a")
+) > 84ciss.manifest
+del test.txt
 
-echo psfreelapse.manifest created successfully!
-timeout /t 2 > nul
+echo 84ciss.manifest created!!
+
+sleep 2
